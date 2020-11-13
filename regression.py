d292aa415f89fa0f5d22c72bc28625fad5dcae0f
Nathan Kolbow <nkolbow@wisc.edu>
2020-11-12 19:02:40 -0600

    LMModel and RegressionModel

--- regression.py
+++ regression.py
@@ -7,22 +7,127 @@ Created on Wed Nov 11 11:20:53 2020
 
 import pandas as pd
 import numpy as np
+from scipy.stats import t
 import random
 
 
-def get_dataset(filename):
-    """
-    INPUT: 
-        filename - a string representing the path to the csv file.
+class RegressionModel():
+    def __init__(self):
+        self._data = None
+        self._lm = None
 
-    RETURNS:
-        An n by m+1 array, where n is # data points and m is # features.
-        The labels y should be in the first column.
-    """
-    df = pd.read_csv(filename)
-    del df["IDNO"]
-    
-    return df.values
+    def read_csv(self, filename, *args, **kwargs):
+        """
+        INPUT: 
+            filename - a string representing the path to the csv file
+            *args    - unnamed arguments to be passed to Pandas' read_csv function
+            **kwargs - named arguments to be passed to Pandas' read_csv function
+
+        RETURNS:
+            An n by m+1 array, where n is # data points and m is # features.
+            The labels y should be in the first column.
+        """
+        self._data = pd.read_csv(filename, *args, **kwargs)
+
+    def get_names(self):
+        print(self._data.columns)
+
+    def lm(self, formula):
+        """
+        INPUT:
+            formula - the formula to fit the model on; currently the only valid
+                      formula take the form "'A' ~ 'B' + 'C' + ..."
+
+        RETURNS:
+            An LMModel
+        """
+        vars = []
+        i = 0
+        while i < len(formula):
+            # Find the next variable
+            while formula[i] != '\'':
+                i += 1
+
+            j = i + 1
+            while formula[j] != '\'':
+                j += 1
+            
+            vars.append(formula[i+1:j]) 
+            i = j + 1
+
+        y = self._data.loc[:, vars[0]].values
+        X = np.append(np.expand_dims(np.repeat(1, self._data.shape[0]), axis=1), self._data.loc[:, vars[1:]].values, axis=1)
+
+        self._lm = LinearModel()
+        self._lm.fit(X, y)
+
+    def print_summary(self, model_type='lm'):
+        if model_type == 'lm':
+            if self._lm is not None:
+                self._lm.print_summary()
+
+
+        
+class LinearModel():
+    def __int__(self):
+        self._betas = None
+
+    def fit(self, X, y):
+        self._n = X.shape[0]
+        self._m = X.shape[1]
+
+        self._betas = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+        mse = 0
+        for sample, actual in zip(X, y):
+            mse += np.power((np.sum(self._betas * sample) - actual), 2)
+        mse /= self._n - self._m
+
+        self._sig_hat_sq = mse
+
+        e = X.dot(self._betas) - y
+        sig_sq = e.T.dot(e) / (self._n - self._m)
+        self._se = np.sqrt(np.diag(sig_sq * np.linalg.inv(X.T.dot(X))))
+
+        self._t = self._betas / self._se
+        self._p = (1 - t.cdf(abs(self._t), self._n - self._m)) * 2
+
+    def summary(self):
+        if self._betas is None:
+            return None
+
+        return {
+                    'n'      : self._n,
+                    'm'      : self._m,
+                    'betas'  : self._betas,
+                    'se'     : self._se,
+                    't'      : self._t,
+                    'p-value': self._p
+               }
+
+    def print_summary(self):
+        if self._betas is None:
+            print("Not fitted model.")
+            return
+
+        summ = self.summary()
+        print('\t\tEstimate\tStd.Error\tt value\tPr(>|t|)')
+        for i in range(summ['m']):
+            if i == 0:
+                print('(Intercept)\t', end='')
+            else:
+                print('No name\t\t', end='')
+
+            p = summ['p-value'][i]
+            stars = ' ' if p > 0.1 else '.' if p > 0.05 else '*' if p > 0.01 else '**' if p > 0.001 else '***'
+            print('%.3e\t%.3e\t%.3f\t%.4f %s' % (summ['betas'][i], summ['se'][i], summ['t'][i], p, stars))
+
+
+
+if __name__ == '__main__':
+    r = RegressionModel()
+    r.read_csv("bodyfat.csv")
+    r.lm("'BODYFAT' ~ 'DENSITY' + 'AGE' + 'KNEE'")
+    r.print_summary()
     
 
 def print_stats(dataset, col):
